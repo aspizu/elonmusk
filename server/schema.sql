417adcd36276e73785120a68fb716143e33b564e
aspizu <aspizu@protonmail.com>
2022-11-19 22:31:35 +0530

    Fix schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,25 +1,25 @@
 CREATE TABLE users (
-  id       INT  PRIMARY KEY,
-  username TEXT NOT NULL UNIQUE,
-  email    TEXT NOT NULL,
-  password TEXT NOT NULL,
-  bio      TEXT NOT NULL,
-  pfp      TEXT NOT NULL,
-  role     INT  NOT NULL
+  id       INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT    NOT NULL UNIQUE,
+  email    TEXT    NOT NULL,
+  password TEXT    NOT NULL,
+  bio      TEXT    NOT NULL,
+  pfp      TEXT    NOT NULL,
+  role     INTEGER NOT NULL
 );
 
 CREATE TABLE posts (
-  id      INT  PRIMARY KEY,
-  user_id INT  NOT NULL,
-  body    TEXT NOT NULL,
-  time    INT  NOT NULL,
+  id      INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  body    TEXT    NOT NULL,
+  time    INTEGER NOT NULL,
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE post_likes (
-  id      INT     PRIMARY KEY,
-  user_id INT     NOT NULL,
-  post_id INT     NOT NULL,
+  id      INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  post_id INTEGER NOT NULL,
   is_like BOOLEAN NOT NULL,
   UNIQUE (post_id, user_id),
   FOREIGN KEY(user_id) REFERENCES users(id),
@@ -27,27 +27,27 @@ CREATE TABLE post_likes (
 );
 
 CREATE TABLE hashtags (
-  id      INT  PRIMARY KEY,
-  hashtag TEXT NOT NULL,
-  post_id INT  NOT NULL,
+  id      INTEGER PRIMARY KEY AUTOINCREMENT,
+  hashtag TEXT    NOT NULL,
+  post_id INTEGER NOT NULL,
   UNIQUE (post_id, hashtag),
   FOREIGN KEY(post_id) REFERENCES posts(id)
 );
 
 CREATE TABLE comments (
-  id      INT  PRIMARY KEY,
-  user_id INT  NOT NULL,
-  post_id INT  NOT NULL,
-  body    TEXT NOT NULL,
-  time    INT  NOT NULL,
+  id      INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  post_id INTEGER NOT NULL,
+  body    TEXT    NOT NULL,
+  time    INTEGER NOT NULL,
   FOREIGN KEY(user_id) REFERENCES users(id),
   FOREIGN KEY(post_id) REFERENCES posts(id)
 );
 
 CREATE TABLE comment_likes (
-  id         INT     PRIMARY KEY,
-  user_id    INT     NOT NULL,
-  comment_id INT     NOT NULL,
+  id         INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id    INTEGER NOT NULL,
+  comment_id INTEGER NOT NULL,
   is_like    BOOLEAN NOT NULL,
   UNIQUE (comment_id, user_id),
   FOREIGN KEY(user_id) REFERENCES users(id),
